2446bbd6874dd36f9cc87ce7c277252029cf1fa8
nahtanojs <nahtanojs@gmail.com>
2014-05-10 10:44:24 +0800

    Changed the criteria for exiting the while loop.
    Since the prince only has 30 days to get the princess, the exit criteria has been changed from getting all "no"s in the list to having tries = 30

--- the princess is in another castle.py
+++ the princess is in another castle.py
@@ -1,11 +1,12 @@
 possBefore = ["yes", "yes", "yes", "yes", "yes", "yes", "yes", "yes", "yes", "yes", "yes", "yes", "yes", "yes", "yes", "yes", "yes"]
 possAfter = ["no", "no", "no", "no", "no", "no", "no", "no", "no", "no", "no", "no", "no", "no", "no", "no", "no"]
 order = []
+tries = 0
 
 print len(possBefore)
 print len(possAfter)
 
-while possBefore != ["no", "no", "no", "no", "no", "no", "no", "no", "no", "no", "no", "no", "no", "no", "no", "no", "no"]:
+while tries < 30:
     check = int(raw_input("Check which room? "))-1
     order.append(check+1)
     possBefore[check] = 'no'
@@ -22,5 +23,11 @@ while possBefore != ["no", "no", "no", "no", "no", "no", "no", "no", "no", "no",
     possBefore = possAfter
     possAfter = ["no", "no", "no", "no", "no", "no", "no", "no", "no", "no", "no", "no", "no", "no", "no", "no", "no"]
     print possBefore
-
+    print tries
+    tries = tries + 1
+    
 print order
+if possBefore == ["no", "no", "no", "no", "no", "no", "no", "no", "no", "no", "no", "no", "no", "no", "no", "no", "no"]:
+    print "You win!"
+else:
+    print "Try again!"
